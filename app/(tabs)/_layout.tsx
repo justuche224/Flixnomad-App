e3eff8ad23e8d1e107a11278c532f0728e50d387
justuche224 <justuche224@gmail.com>
2024-06-25 09:08:25 +0100

    about to try download feature

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,60 +1,68 @@
 import { Tabs } from "expo-router";
 import React from "react";
 import { AntDesign } from "@expo/vector-icons";
-import { TabBarIcon } from "@/components/navigation/TabBarIcon";
-import TabBar from "@/components/navigation/TabBar";
+// import TabBar from "@/components/navigation/TabBar";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
-    const colorScheme = useColorScheme();
-    //      tabBar={(props) => <TabBar {...props} />}
+  const colorScheme = useColorScheme();
+  //      tabBar={(props) => <TabBar {...props} />}
 
-    return (
-        <Tabs
-            screenOptions={{
-                tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-                headerShown: false,
-                tabBarStyle: { backgroundColor: "black" },
-                tabBarShowLabel: false,
-            }}
-        >
-            <Tabs.Screen
-                name="index"
-                options={{
-                    title: "Home",
-                    tabBarIcon: ({ color, focused }) => (
-                        <AntDesign name="home" size={28} color={color} />
-                    )
-                }}
-            />
-            <Tabs.Screen
-                name="movies"
-                options={{
-                    title: "Movies",
-                    tabBarIcon: ({ color, focused }) => (
-                        <AntDesign name="playcircleo" size={28} color={color} />
-                    )
-                }}
-            />
-            <Tabs.Screen
-                name="series"
-                options={{
-                    title: "Series",
-                    tabBarIcon: ({ color, focused }) => (
-                        <AntDesign name="videocamera" size={28} color={color} />
-                    )
-                }}
-            />
-            <Tabs.Screen
-                name="genres"
-                options={{
-                    title: "Genres",
-                    tabBarIcon: ({ color, focused }) => (
-                        <AntDesign name="bars" size={28} color={color} />
-                    )
-                }}
-            />
-        </Tabs>
-    );
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarStyle: { backgroundColor: "black", borderTopColor: "black" },
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <AntDesign name="home" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="movies"
+        options={{
+          title: "Movies",
+          tabBarIcon: ({ color, focused }) => (
+            <AntDesign name="playcircleo" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="downloads"
+        options={{
+          title: "Downloads",
+          tabBarIcon: ({ color, focused }) => (
+            <AntDesign name="download" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="series"
+        options={{
+          title: "Series",
+          tabBarIcon: ({ color, focused }) => (
+            <AntDesign name="videocamera" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="genres"
+        options={{
+          title: "Genres",
+          tabBarIcon: ({ color, focused }) => (
+            <AntDesign name="bars" size={28} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
